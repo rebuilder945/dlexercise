37a517ada88a6cd324f6d5f9b4b5c6303e7efa44
rebuilder945 <1278950828@qq.com>
2022-02-19 22:48:00 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import torch
+import numpy
+import matplotlib
+
+a = 4
+b = 4
+a, b = b, a
+print(1)
+
+# 这个写法中，__name__表示该文件的文件名，当该模块直接执行时，其值为 文件名.py， 当该模块被调用而执行时，其值为 文件名;
+# 而__main__为一常量， 其值始终为 文件名.py;
+# 因此在直接执行时，该语句后的语句也执行
+# 而调用执行时，该语句后的语句不执行
+if __name__ == '__main__':
+    print(2)
+
+
+# print(torch.__version__)
+# print('gpu', torch.cuda.is_available())
